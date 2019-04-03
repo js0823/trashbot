d27f443ce8726a0a21c7e234d6315eb30e5809ab
Jongseo Yoon <js0823@gmail.com>
2019-04-03 19:18:15 -0400

    Working on YOLO

--- src/yolo.py
+++ src/yolo.py
@@ -557,3 +557,32 @@ def load_weights(variables, file_name):
             assign_ops.append(tf.assign(conv_var, var_weights))
 
     return assign_ops
+
+img_names = ['dog.jpg']
+for img in img_names: display(Image.open(img))
+
+batch_size = len(img_names)
+batch = load_images(img_names, model_size=_MODEL_SIZE)
+class_names = load_class_names('../input/coco.names')
+n_classes = len(class_names)
+max_output_size = 10
+iou_threshold = 0.5
+confidence_threshold = 0.5
+
+model = Yolo_v3(n_classes=n_classes, model_size=_MODEL_SIZE,
+                max_output_size=max_output_size,
+                iou_threshold=iou_threshold,
+                confidence_threshold=confidence_threshold)
+
+inputs = tf.placeholder(tf.float32, [batch_size, 416, 416, 3])
+
+detections = model(inputs, training=False)
+
+model_vars = tf.global_variables(scope='yolo_v3_model')
+assign_ops = load_weights(model_vars, '../input/yolov3.weights')
+
+with tf.Session() as sess:
+    sess.run(assign_ops)
+    detection_result = sess.run(detections, feed_dict={inputs: batch})
+    
+draw_boxes(img_names, detection_result, class_names, _MODEL_SIZE)
