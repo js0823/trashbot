de3db7a57bbf3d7c884999eb963d699eeb7fd3d6
Jongseo Yoon <js0823@gmail.com>
2019-04-12 17:22:29 -0400

    Python script to generate YOLO dataset train and test

---
+++ data/generate_yolo_dataset.py
@@ -0,0 +1,21 @@
+import glob, os
+
+dataset_path = '/home/js0823/github-repo/trashbot/data/images'
+
+percentage_test = 10
+
+file_train = open('train.txt', 'w')
+file_test = open('test.txt', 'w')
+
+counter = 1
+index_test = round(100 / percentage_test)
+
+for pathAndFilename in glob.iglob(os.path.join(dataset_path, "*.jpg")):
+    title, ext = os.path.splitext(os.path.basename(pathAndFilename))
+
+    if counter == index_test + 1:
+        counter = 1
+        file_test.write(dataset_path + "/" + title + '.jpg' + "\n")
+    else:
+        file_train.write(dataset_path + "/" + title + '.jpg' + "\n")
+        counter = counter + 1
