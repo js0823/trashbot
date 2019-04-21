6d90b2acb36134ee266aa76e14fe15661c88cc52
Jongseo Yoon <js0823@gmail.com>
2019-04-21 02:19:08 -0400

    Adding python version of roamer

--- trashbot_launch/src/trashbot_roamer.py
+++ trashbot_launch/src/trashbot_roamer.py
@@ -1,123 +1,55 @@
 #!/usr/bin/env python
+
 import rospy
-import rospkg
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+import actionlib
+import tf
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 
-# import sys
-# sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
+PI = 3.14159265359
 
-from ctypes import *
-import math
-import random
-import cv2
-import colorsys
-import numpy as np
-import os
-import shutil
-import copy
-# import argparse
+def move_turtlebot(x, y, yaw):
+    client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+    client.wait_for_server()
+    goal = MoveBaseGoal()
+    print("Target Position: {}, {}".format(x, y))
 
-from darknet import *
-from yolo_ros_msgs.msg import YoloBox
-from yolo_ros_msgs.msg import YoloBoxes
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.header.frame_id = "/map"
 
+    goal.target_pose.pose.position.x = x
+    goal.target_pose.pose.position.y = y
+    goal.target_pose.pose.position.z = 0.0
+    goal.target_pose.pose.orientation = tf.createQuaternionMsgFromYaw(yaw)
 
-class TrashbotRoamerNode:
+    client.send_goal(goal)
+    wait = client.wait_for_result()
+    if not wait:
+        rospy.logerr("Action server not available!")
+        rospy.signal_shutdown("Action server not available!")
+    else:
+        return client.get_result()
 
-    def __init__(self):
-
-        rospy.init_node('trashbot_roamer', anonymous=True)
-
-        file_path = rospkg.RosPack.get_path('trashbot_launch')
-        net_file = file_path + rospy.get_param('~net_file', "/../darknet/cfg/yolov3.cfg")
-        weights_file = file_path + rospy.get_param('~weights_file', "/../darknet/yolov3.weights")
-        meta_file = file_path + rospy.get_param('~meta_file', "/../darknet/cfg/coco.data")
-        names_file = file_path + rospy.get_param('~names_file', "/../darknet/data/coco.names")
-        input_image_topic = rospy.get_param('~input_image_topic', "/camera/rgb/image_raw")
-        output_image_topic = rospy.get_param('~output_image_topic', "/camera/rgb/yolo_image_output")
-        output_topic = rospy.get_param('~output_topic', "/yolo/recognition_result")
-        cropping_area = rospy.get_param('~cropping_area', None)
-
-        if cropping_area is not None:
-            cropping_area = [[int(x) for x in ss.lstrip('[').split(' ')][0] for ss in cropping_area.rstrip(']').split(',')]
-        self.cropping_area = cropping_area
-
-        if os.path.isdir('data'):
-            shutil.rmtree('data')
-
-        os.mkdir("data")
-        shutil.copy(names_file, "data")
-        self.net = load_net(net_file.encode('utf-8'), weights_file.encode('utf-8'), 0)
-        self.meta = load_meta(meta_file.encode('utf-8'))
-        shutil.rmtree("data")
-
-        self.bridge = CvBridge()
-        self.image_sub = rospy.Subscriber(input_image_topic, Image, self.image_callback, queue_size=1, buff_size=2**24)
-        self.result_image_pub = rospy.Publisher(output_image_topic, Image, queue_size=1)
-        self.result_pub = rospy.Publisher(output_topic, YoloBoxes, queue_size=1)
-
-        self.color = (0,255,0)
-        rospy.loginfo("Initialized TrashbotRoamerNode")
-
-    def image_callback(self, data):
-        try:
-            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-            self.darknet_detection(cv_image)
-        except CvBridgeError as e:
-            print(e)
-
-    def darknet_detection(self, cv_image):
-
-        # cv_image = cv2.resize(cv_image, None, fx=0.5, fy=0.5)
-
-        yolo_boxes = YoloBoxes()
-        yolobox_list = []
-
-        cv_image_ = copy.deepcopy(cv_image)
-        
-        if self.cropping_area is not None:
-            cv_image = cv_image[self.cropping_area[0]:self.cropping_area[1],
-                                self.cropping_area[2]:self.cropping_area[3]]
-
-        r = detect_np(self.net, self.meta, cv_image)
-        for i in r:
-            x, y, w, h = i[2][0], i[2][1], i[2][2], i[2][3]
-            xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
-            probability = round(i[1], 4)
-            label = i[0].decode()
-
-            if self.cropping_area is not None:
-                xmin += self.cropping_area[2]
-                xmax += self.cropping_area[2]
-                ymin += self.cropping_area[0]
-                ymax += self.cropping_area[0]
-
-            yolobox = YoloBox()
-            yolobox.probability = probability
-            yolobox.label = label
-            yolobox.xmin = xmin
-            yolobox.ymin = ymin
-            yolobox.xmax = xmax
-            yolobox.ymax = ymax
-
-            pt1 = (xmin, ymin)
-            pt2 = (xmax, ymax)
-            cv2.rectangle(cv_image_, pt1, pt2, self.color, 2)
-            cv2.putText(cv_image_, label + " [" + str(probability*100) + "%]",
-                        (pt1[0]+2, pt1[1]+25), cv2.FONT_HERSHEY_SIMPLEX, 0.8, self.color, 3)
-
-            yolobox_list.append(yolobox)
-
-        yolo_boxes.dark_array = yolobox_list
-        self.result_image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_, "bgr8"))
-        self.result_pub.publish(yolo_boxes)
-
-
-if __name__ == "__main__":
-
-    TrashbotRoamerNode()
+if __name__ == '__main__':
     try:
-        rospy.spin()
-    except KeyboardInterrupt:
-         print("Shutting down")
+        rospy.init_node('move_turtlebot_py')
+        home_location = [5.65, 13.8, 0.0]
+
+        num_locations = 2
+        locations = [[21.8,13.9,0.0], [5.8,13.9,0.0]]
+
+        location_names = ["Collab Room Left", "Kitchen Left"]
+        x1 = y1 = x2 = y2 = x3 = y3 = x4 = y4 = 0
+        start_index = 1
+        goal_index = start_index
+
+        while not rospy.is_shutdown():
+            print("Target Location : {}".format(location_names[goal_index]))
+            for p in range(3):
+                result = move_turtlebot(locations[goal_index][0], locations[goal_index][1], locations[goal_index][2] + (p + 1) * PI / 2)
+            if result:
+                rospy.loginfo("Goal execution done.")
+            goal_index += 1
+            if goal_index >= num_locations:
+                goal_index = 0
+    except rospy.ROSInternalException:
+        rospy.loginfo("Roamer finished.")
